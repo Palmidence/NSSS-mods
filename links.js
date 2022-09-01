efa5799879baafbca1a1087e736d8897ae54694f
Vice <paraspooder@gmail.com>
2022-09-01 05:24:50 -0400

    Remove version from bottom to remove clutter
    Tags for versions will likely go on the top right of cards

--- links.js
+++ links.js
@@ -36,8 +36,7 @@ const resources = createApp({
             </div>
             <div class="card-footer">
                 <a :href="resource.url" style="color: black;"><i class="fa-solid fa-2x fa-download" style="float: left;"></i></a>
-                <i class="fa-solid fa-2x fa-person" data-toggle="tooltip" :title="resource.author" style="float: none;"></i>
-                <i class="fa-solid fa-2x fa-tag" data-toggle="tooltip" :title="resource.version" style="float: right;"></i>
+                <i class="fa-solid fa-2x fa-person" data-toggle="tooltip" :title="resource.author" style="float: right;"></i>
             </div>
         </div>
     </div>
